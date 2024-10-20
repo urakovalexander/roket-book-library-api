6ef01e630262ad33012ebad2c5d0e1ae2a1d847a
urakovalexander <urakov.alexander@gmail.com>
2024-10-21 00:25:42 +0500

    initial project structure and create README.md, Database.php

---
+++ README.md
@@ -0,0 +1,6 @@
+Example of an environment variables file .env
+DB_HOST=your_db_host
+DB_NAME=your_db_name
+DB_USER=your_db_user
+DB_PASS=your_db_password
+JWT_SECRET=your_secret_key
---
+++ src/Config/Database.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Alexanderurakov\RoketBookLibraryApi\Config;
+
+use PDO;
+use PDOException;
+use Dotenv\Dotenv;
+
+class Database {
+    private static $instance = null;
+    private $conn;
+    private $host;
+    private $db_name;
+    private $username;
+    private $password;
+
+    private function __construct() {
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+        $dotenv->load();
+
+        // Загрузка переменных окружения для подключения к базе данных
+        $this->host = $_ENV['DB_HOST'];
+        $this->db_name = $_ENV['DB_NAME'];
+        $this->username = $_ENV['DB_USER'];
+        $this->password = $_ENV['DB_PASS'];
+
+        // Попытка установить соединение с базой данных
+        try {
+            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->db_name", $this->username, $this->password);
+            // Установка режима обработки ошибок
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $exception) {
+            // Обработка ошибок соединения
+            echo "Connection error: " . $exception->getMessage();
+        }
+    }
+
+    public static function getInstance(): PDO
+    {
+        if (self::$instance === null) {
+            self::$instance = new Database();
+        }
+
+        return self::$instance->conn;
+    }
+}
